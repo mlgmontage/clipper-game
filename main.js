9f2a9e8f4c2fdf37eb90f6dfa88f41d9f71bc1ed
mlgmontage <kettleofrassel1@gmail.com>
2020-08-04 11:01:58 +0500

    setup UIkit && autoClipper

--- main.js
+++ main.js
@@ -1,53 +1,62 @@
 let clipper = 10,
   budget = 110,
   wire = 510,
-  cost = 0.1
+  cost = 0.1,
+  autoClipper = 0;
 
 // DOM
-const clipperElm = document.querySelector('#clipper')
-const budgetElm = document.querySelector('#budget')
-const wireElm = document.querySelector('#wire')
-const costElm = document.querySelector('#cost')
+const clipperElm = document.querySelector("#clipper");
+const budgetElm = document.querySelector("#budget");
+const wireElm = document.querySelector("#wire");
+const costElm = document.querySelector("#cost");
+const autoClipperElm = document.querySelector("#autoClipper");
 
 // DOM controllers
-const makeClipperElm = document.querySelector('#makeClipper')
-const buyWireElm = document.querySelector('#buyWire')
+const makeClipperElm = document.querySelector("#makeClipper");
+const buyWireElm = document.querySelector("#buyWire");
 
 // Event
-makeClipperElm.addEventListener('click', () => {
-  clipper += 1
-  wire -= 3
-  update()
-})
+makeClipperElm.addEventListener("click", () => {
+  clipper += 1;
+  wire -= 3;
+  update();
+});
 
-buyWireElm.addEventListener('click', () => {
+buyWireElm.addEventListener("click", () => {
   // NOTE: magic constant
   // 100 inch of wire costs 10$
   if (budget <= 0 || budget < 10) {
+    UIkit.notification({
+      message: "You don't have enough money to buy wire",
+      status: "warning",
+      pos: "top-right",
+      timeout: 3000,
+    });
   } else {
-    wire += 100
-    budget -= 10
+    wire += 100;
+    budget -= 10;
   }
-  update()
-})
+  update();
+});
 
 // Game loop
 setInterval(() => {
   if (clipper <= 0) {
   } else {
-    clipper -= 1
-    budget += cost
-    update()
+    clipper -= 1;
+    budget += cost;
+    update();
   }
-}, 1000)
+}, 1000);
 
 const update = () => {
   // Udate
-  clipperElm.textContent = clipper
-  budgetElm.textContent = budget.toFixed(2)
-  wireElm.textContent = wire
-  costElm.textContent = cost
-}
+  clipperElm.textContent = clipper;
+  budgetElm.textContent = budget.toFixed(2);
+  wireElm.textContent = wire;
+  costElm.textContent = cost;
+  autoClipperElm.textContent = autoClipper;
+};
 
 // Setup
-update()
+update();
